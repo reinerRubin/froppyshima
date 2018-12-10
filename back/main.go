c60fcb1aa761e6f68821c7310a4b4f767b462b8e
tg <tolstov.georgij@gmail.com>
2018-12-10 15:45:50 +0300

    [config, devops] added the config, add a volume for boltdb

--- back/main.go
+++ back/main.go
@@ -1,25 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"syscall"
 
+	"github.com/reinerRubin/froppyshima/back/internal/config"
 	"github.com/reinerRubin/froppyshima/back/internal/server"
 )
 
 func main() {
+	if err := runApp(); err != nil {
+		log.Fatalf("app is terminated with error: %s", err)
+	}
+}
+
+func runApp() error {
 	log.Println("welcome to `froppyshima' the game")
 
-	server := server.New()
-	server.Start()
+	config, err := config.New()
+	if err != nil {
+		return fmt.Errorf("cant init cfg: %s", err)
+	}
+
+	server := server.New(config)
+	if err := server.Start(); err != nil {
+		return err
+	}
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 
-	server.Stop()
+	if err := server.Stop(); err != nil {
+		return err
+	}
 
 	log.Println("see you space cowboy!")
+	return nil
 }
